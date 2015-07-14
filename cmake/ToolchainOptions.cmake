22df97116f8ccef1e736f51fde2e3fd76c52a22a
ahueck <hueck.alexander@gmail.com>
2015-07-14 22:42:15 +0200

    Changed build, as it was not working for binary install (LLVM cmake path was set wrongly in LLVMConfig.cmake by maintainer)

--- cmake/ToolchainOptions.cmake
+++ cmake/ToolchainOptions.cmake
@@ -2,29 +2,15 @@ set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules)
 
 #setup of all necessary include/lib dirs for the development of a clang based tool
 find_package(LLVM REQUIRED)
-  message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION} ${LLVM_LIBRARY_DIRS}")
-
-#find_package(LLVM REQUIRED CONFIG)
-#message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
-#message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
-
 find_package(Clang REQUIRED)
 
-if(NOT CLANG_FOUND)# OR NOT LLVM_FOUND)
-	message(WARNING "Detection of Clang. Status: " CLANG_FOUND)
-	#message(WARNING "Detection of LLVM. Status: " LLVM_FOUND)
-	message(FATAL_ERROR "Aborting.")
+if(NOT CLANG_FOUND)
+	message(FATAL_ERROR "Detection of Clang failed.")
 else()
 	include_directories(${LLVM_INCLUDE_DIRS})
 	add_definitions(${LLVM_DEFINITIONS})
 	link_directories(${LLVM_LIBRARY_DIRS})
-
 	set(TOOLCHAIN_LINK_LIB ${CLANG_LIBS} ${LLVM_LIBS} ${LLVM_SYSTEM_LIBS}) 
-	#include_directories(${LLVM_INCLUDE_DIR})
-	#include_directories(${CLANG_INCLUDE_DIRS})
-	#link_directories(${LLVM_LIB_DIR})
-	#set(TOOLCHAIN_LINK_LIB ${CLANG_LIBS} ${LLVM_LIBS_CORE} ${LLVM_LDFLAGS}) 
-	#set(CXX_ONLY_FLAGS ${CXX_ONLY_FLAGS} "${LLVM_COMPILE_FLAGS}")
 endif()
 
 # Misc. find out if clang compiler is used
@@ -34,7 +20,8 @@ endif()
 
 #setup general compiler options
 if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX)
-	set(CXX_ONLY_FLAGS "${CXX_ONLY_FLAGS} ${LLVM_COMPILE_FLAGS} -fno-rtti") 
+    # Ignore for now: ${LLVM_COMPILE_FLAGS}
+	set(CXX_ONLY_FLAGS "-std=c++11 -Wall -Wextra -pedantic -Wunreachable-code -Wwrite-strings -Wpointer-arith -Wcast-align -Wcast-qual -Wno-unused-parameter -fPIC -fexceptions -fno-rtti ${CXX_ONLY_FLAGS}")
 
 	# Release build
 	set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
@@ -45,7 +32,7 @@ if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX)
 	# Release build optimized for size
 	set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os")
 
-	set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} ${CXX_ONLY_FLAGS} ${CMAKE_CXX_FLAGS}")
+	set(CMAKE_CXX_FLAGS  "${CXX_ONLY_FLAGS} ${COMMON_FLAGS} ${CMAKE_CXX_FLAGS}")
 endif()
 
 # set default build type
@@ -79,7 +66,7 @@ else()
 	set(ALL_CXX_FLAGS ${CMAKE_CXX_FLAGS_DEBUG})
 endif()
 
-message(STATUS "Build Type: " "${CMAKE_BUILD_TYPE}")
+message(STATUS "Build Type: ${CMAKE_BUILD_TYPE}")
 message(STATUS "Compiler Flags (Modus): ${ALL_CXX_FLAGS}")
 message(STATUS "Compiler Flags (cmake): ${CMAKE_CXX_FLAGS}")
 message(STATUS "Found Clang: ${CLANG_FOUND}")
--- cmake/modules/FindLLVM.cmake
+++ cmake/modules/FindLLVM.cmake
@@ -1,27 +1,8 @@
-# Detect LLVM and set various variable to link against the different component of LLVM
-#
-# NOTE: This is a modified version of the module originally found in the OpenGTL project
-# at www.opengtl.org
-#
-# LLVM_BIN_DIR : directory with LLVM binaries
-# LLVM_LIB_DIR : directory with LLVM library
-# LLVM_INCLUDE_DIR : directory with LLVM include
-#
-# LLVM_COMPILE_FLAGS : compile flags needed to build a program using LLVM headers
-# LLVM_LDFLAGS : ldflags needed to link
-# LLVM_LIBS_CORE : ldflags needed to link against a LLVM core library
-# LLVM_LIBS_JIT : ldflags needed to link against a LLVM JIT
-# LLVM_LIBS_JIT_OBJECTS : objects you need to add to your source when using LLVM JIT
-
-
-
-
-
 ##
 ## Setup for llvm-config executable, and important llvm vars
 ##
 ## Out:
-##  Vars: ${LLVM_ROOT_DIR}
+##  Var: 
 ##  Exe: ${LLVM_CONFIG}
 function(setup_llvm)
   set(llvm_config_names llvm-config-3.7
@@ -31,7 +12,7 @@ function(setup_llvm)
 
   find_program(LLVM_CONFIG
       NAMES ${llvm_config_names}
-      PATHS /usr/local/bin /opt/local/bin ${LLVM_ROOT_DIR}/bin NO_DEFAULT_PATH
+      PATHS /usr/local/bin /opt/local/bin ${LLVM_ROOT_DIR}/bin
       DOC "llvm-config executable.")
 
   if(LLVM_CONFIG)
@@ -44,7 +25,6 @@ function(setup_llvm)
     COMMAND ${LLVM_CONFIG} --prefix
     OUTPUT_VARIABLE LLVM_ROOT_DIR
     OUTPUT_STRIP_TRAILING_WHITESPACE)
-
   set(LLVM_ROOT_DIR ${LLVM_ROOT_DIR} PARENT_SCOPE)
   set(LLVM_CONFIG ${LLVM_CONFIG} PARENT_SCOPE)
 endfunction()
@@ -55,17 +35,18 @@ function(set_llvm_cppflags)
     OUTPUT_VARIABLE llvm_cxxflags
     OUTPUT_STRIP_TRAILING_WHITESPACE)
   
-  string(REGEX REPLACE "(-D[^ ]*)|(-U[^ ]*)|(-I[^ ]*)" "" llvm_cxxflags ${llvm_cxxflags})
+  string(REGEX REPLACE "-D[^ ]*|-U[^ ]*|-I[^ ]*|-g[^ ]*|-[oO][^ ]*" "" llvm_cxxflags ${llvm_cxxflags})
+  string(REGEX REPLACE "[ ]+" " " llvm_cxxflags ${llvm_cxxflags})
   set(LLVM_COMPILE_FLAGS ${llvm_cxxflags} PARENT_SCOPE)
 endfunction()
 
 function(set_llvm_lib_path)
   execute_process(
-    COMMAND ${LLVM_CONFIG} --ldflags
+    COMMAND ${LLVM_CONFIG} --libdir
     OUTPUT_VARIABLE llvm_ldflags
     OUTPUT_STRIP_TRAILING_WHITESPACE)
   
-  set(LLVM_LD_FLAGS ${llvm_ldflags} PARENT_SCOPE)
+  set(LLVM_LIBRARY_DIRS ${llvm_ldflags} PARENT_SCOPE)
 endfunction()
 
 function(set_llvm_system_libs)
@@ -77,62 +58,58 @@ function(set_llvm_system_libs)
   set(LLVM_SYSTEM_LIBS ${llvm_system_libs} PARENT_SCOPE)
 endfunction()
 
+function(set_llvm_include_dir)
+  execute_process(
+    COMMAND ${LLVM_CONFIG} --includedir
+    OUTPUT_VARIABLE llvm_includdir
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  
+  set(LLVM_INCLUDE_DIRS ${llvm_includdir} PARENT_SCOPE)
+endfunction()
+
+function(set_llvm_definitions)
+  execute_process(
+    COMMAND ${LLVM_CONFIG} --cppflags
+    OUTPUT_VARIABLE llvm_definitions
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  
+  string(REGEX REPLACE "-I[^ ]*" "" llvm_definitions ${llvm_definitions})
+  string(REGEX REPLACE "[ ]+" " " llvm_definitions ${llvm_definitions})
+  set(LLVM_DEFINITIONS ${llvm_definitions} PARENT_SCOPE)
+endfunction()
+
+function(set_llvm_libs_all)
+  execute_process(
+    COMMAND ${LLVM_CONFIG} --libs
+    OUTPUT_VARIABLE llvm_libs
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REGEX REPLACE " -" ";-" llvm_libs ${llvm_libs})
+  set(LLVM_LIBS ${llvm_libs} PARENT_SCOPE)
+endfunction()
 
 setup_llvm()
 
 ## Use existing CMake feature provided by LLVM
 ##  Vars (partial): ${LLVM_INCLUDE_DIRS}, ${LLVM_LIBRARY_DIRS}, ${LLVM_DEFINITIONS}
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${LLVM_ROOT_DIR}/share/llvm/cmake")
-include(LLVMConfig)
-message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION} ${LLVM_LIBRARY_DIRS}")
+#set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${LLVM_ROOT_DIR}/share/llvm-3.7/cmake")
+#set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${LLVM_ROOT_DIR}/share/llvm-3.6/cmake")
+#set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${LLVM_ROOT_DIR}/share/llvm-3.5/cmake")
+#set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${LLVM_ROOT_DIR}/share/llvm/cmake")
+#message(STATUS ${CMAKE_MODULE_PATH})
+#include(LLVMConfig)
+# Alternative: seems buggy on some (binary) installs
+#find_package(LLVM REQUIRED CONFIG)
 
 # FIXME does not work on this plattform (CLang 3.5.0)
 #llvm_map_components_to_libnames(LLVM_LIBS support core)
-set(LLVM_LIBS LLVMCore;LLVMSupport;LLVMOption;LLVMAsmParser;LLVMBitReader;LLVMIRReader;LLVMObject;LLVMMC;LLVMMCParser;LLVMCodeGen;LLVMExecutionEngine;LLVMInterpreter)
+
+#set(LLVM_LIBS LLVMOption;LLVMAsmParser;LLVMBitReader;LLVMIRReader;LLVMObject;LLVMMC;LLVMMCParser;LLVMCodeGen;LLVMExecutionEngine;LLVMInterpreter;LLVMCore;LLVMSupport)
+#set(LLVM_LIBS LLVMOption;LLVMAsmParser;LLVMBitReader;LLVMIRReader;LLVMObject;LLVMMCParser;LLVMInterpreter;LLVMExecutionEngine;LLVMMC;LLVMCore;LLVMSupport)
+#set(LLVM_LIBS LLVMOption;LLVMAsmParser;LLVMBitReader;LLVMIRReader;LLVMObject;LLVMMCParser;LLVMMC;LLVMCore;LLVMSupport)
+set(LLVM_LIBS LLVMIRReader;LLVMObject;LLVMMCParser;LLVMMC;LLVMBitReader;LLVMAsmParser;LLVMCore;LLVMOption;LLVMSupport)
+#set_llvm_libs_all()
 set_llvm_cppflags()
-#set_llvm_lib_path()
+set_llvm_lib_path()
 set_llvm_system_libs()
-
-return()
-
-
-
-if(LLVM_INCLUDE_DIR)
-  set(LLVM_FOUND TRUE)
-else()
-	find_program(LLVM_CONFIG_EXECUTABLE
-	  NAMES llvm-config-3.5 llvm-config 
-	  PATHS /usr/local/bin /opt/local/bin
-	)
-
-	if(NOT LLVM_CONFIG_EXECUTABLE)
-		message(FATAL_ERROR "Could not find program llvm-config.")
-	endif()
-
-	macro(find_llvm_libs LLVM_CONFIG_EXECUTABLE _libname_ LIB_VAR OBJECT_VAR)
-	  exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --libs ${_libname_}  OUTPUT_VARIABLE ${LIB_VAR} )
-	  string(REGEX MATCHALL "[^ ]*[.]o[ $]"  ${OBJECT_VAR} ${${LIB_VAR}})
-	  separate_arguments(${OBJECT_VAR})
-	  string(REGEX REPLACE "[^ ]*[.]o[ $]" ""  ${LIB_VAR} ${${LIB_VAR}})
-	endmacro()
-
-	exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --bindir OUTPUT_VARIABLE LLVM_BIN_DIR )
-	exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --libdir OUTPUT_VARIABLE LLVM_LIB_DIR )
-	#MESSAGE(STATUS "LLVM lib dir: " ${LLVM_LIB_DIR})
-	exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --includedir OUTPUT_VARIABLE LLVM_INCLUDE_DIR )
-	exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --cxxflags  OUTPUT_VARIABLE LLVM_COMPILE_FLAGS )
-	execute_process(COMMAND ${LLVM_CONFIG_EXECUTABLE} --ldflags  OUTPUT_VARIABLE LLVM_LDFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-	#--system-libs is new in llvm 3.5
-	execute_process(COMMAND ${LLVM_CONFIG_EXECUTABLE} --system-libs  OUTPUT_VARIABLE LLVM_LDFLAGS2 OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-	string(REPLACE "\n" " " LLVM_LDFLAGS ${LLVM_LDFLAGS} " " ${LLVM_LDFLAGS2})
-	string(REGEX REPLACE "(\n|\r\n)$" "" LLVM_LDFLAGS "${LLVM_LDFLAGS}")
-	exec_program(${LLVM_CONFIG_EXECUTABLE} ARGS --libs      OUTPUT_VARIABLE LLVM_LIBS_CORE )
-	find_llvm_libs(${LLVM_CONFIG_EXECUTABLE} "jit native" LLVM_LIBS_JIT LLVM_LIBS_JIT_OBJECTS )
-	#STRING(REPLACE " -lLLVMCore -lLLVMSupport -lLLVMSystem" "" LLVM_LIBS_JIT ${LLVM_LIBS_JIT_RAW})
-
-	if(LLVM_INCLUDE_DIR)
-	  set(LLVM_FOUND TRUE)
-	else()
-		set(LLVM_FOUND FALSE)
-	endif()
-endif()
+set_llvm_include_dir()
+set_llvm_definitions()
