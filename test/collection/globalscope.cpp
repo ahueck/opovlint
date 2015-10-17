3f7f31f00b4d0c9bb68b7e14c2efbf5d642907ae
ahueck <hueck.alexander@gmail.com>
2015-10-17 13:01:52 +0200

    GlobalScope should find function-calls with only ::-specifier now.

---
+++ test/collection/globalscope.cpp
@@ -0,0 +1,35 @@
+#pragma clang diagnostic ignored "-Wunused-value"
+typedef double scalar;
+
+class X;
+int a = 1;
+scalar b = 1.0;
+double c = 2.0;
+
+void f(const X& x);
+
+class X {
+public:
+	friend void f(const X& x) {  }
+};
+
+void g(scalar a);
+void h(double b);
+
+namespace ns {
+void f(const X& x);
+void g(scalar z);
+void f();
+void call() {
+	::f(X());
+	ns::f(X());
+	f();
+	::a;
+	::b;
+  	::g(scalar(1.0));
+	::g(::b);
+	::g(::b*scalar(1));
+	g(scalar(1.0));
+	ns::g(scalar(1.0));
+}
+}
