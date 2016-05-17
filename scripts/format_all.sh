bbae5ac8587b70447e8733da9c4a9b078ccb3ddd
Alexander Hück <hueck.alexander@gmail.com>
2016-05-17 16:24:51 +0200

    Format script tries several executable names of clang-format.

--- scripts/format_all.sh
+++ scripts/format_all.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-find -regextype posix-egrep -regex ".*\.(h|cpp|hpp)" \( -path "./include/*" -o -path "./src/*" \) -not -path "./include/external/*" -exec clang-format -i -style=file -fallback-style=none {} \;
+format=clang-format
+
+# clang-format-3.6 does not support all file entries 
+# TODO check compatibility with clang-format-3.7 
+for v in 3.8 3.7 ; do
+  if [[ $(type -p clang-format-"$v") ]] ; then  
+    format=clang-format-"$v"
+    break
+  fi
+done
+
+find -regextype posix-egrep -regex ".*\.(h|cpp|hpp)" \
+         \( -path "./include/*" -o -path "./src/*" \) -not -path "./include/external/*" \
+         -exec "$format" -i -style=file -fallback-style=none {} \;
 
