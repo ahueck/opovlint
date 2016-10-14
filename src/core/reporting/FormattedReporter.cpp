a3f96d0121104b7ffb79f34c1d241dd7d7f984e9
Alexander Hück <hueck.alexander@gmail.com>
2016-10-14 17:25:54 +0200

    Missing Reporter file

---
+++ src/core/reporting/FormattedReporter.cpp
@@ -0,0 +1,30 @@
+/*
+ * FormattedReporter.cpp
+ *
+ *  Created on: Oct 12, 2016
+ *      Author: ahueck
+ */
+
+#include <core/reporting/FormattedReporter.h>
+#include <core/configuration/Configuration.h>
+
+namespace opov {
+
+FormattedReporter::FormattedReporter(const Configuration* config) {
+  std::string format_string;
+  config->getValue("global:format", format_string, "%f:%ls:%cs: %m @ %s");
+  formatter = build_formatter(std::begin(format_string), std::end(format_string));
+}
+
+void FormattedReporter::addIssues(const IssueVector& issues) {
+  std::ostringstream out_ss;
+  for (auto& issue : issues) {
+    for (auto& format_fn : formatter) {
+      out_ss << format_fn(issue);
+    }
+    out_ss << "\n";
+  }
+  std::cout << out_ss.str();
+}
+
+} /* namespace opov */
