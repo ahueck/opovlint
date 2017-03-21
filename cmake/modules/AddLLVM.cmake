d225f866489544a8e5480f62720ae05887f1a13a
Alexander Hück <hueck.alexander@gmail.com>
2017-03-21 15:57:58 +0100

    cmake target to execute lit

---
+++ cmake/modules/AddLLVM.cmake
@@ -0,0 +1,54 @@
+function(add_lit_target target comment)
+  cmake_parse_arguments(ARG "" "" "PARAMS;DEPENDS;ARGS" ${ARGN})
+  set(LIT_ARGS "${ARG_ARGS} ${LLVM_LIT_ARGS}")
+  separate_arguments(LIT_ARGS)
+  if (NOT CMAKE_CFG_INTDIR STREQUAL ".")
+    list(APPEND LIT_ARGS --param build_mode=${CMAKE_CFG_INTDIR})
+  endif ()
+  find_program(LIT_COMMAND 
+               NAMES llvm-lit llvm-lit-4.0 llvm-lit-3.8 llvm-lit-3.7 llvm-lit-3.6)
+  list(APPEND LIT_COMMAND ${LIT_ARGS})
+  foreach(param ${ARG_PARAMS})
+    list(APPEND LIT_COMMAND --param ${param})
+  endforeach()
+  if (ARG_UNPARSED_ARGUMENTS)
+    add_custom_target(${target}
+      COMMAND ${LIT_COMMAND} ${ARG_UNPARSED_ARGUMENTS}
+      COMMENT "${comment}"
+      USES_TERMINAL
+      )
+  else()
+    add_custom_target(${target}
+      COMMAND ${CMAKE_COMMAND} -E echo "${target} does nothing, no tools built.")
+    message(STATUS "${target} does nothing.")
+  endif()
+  if (ARG_DEPENDS)
+    message(We have dependency "${ARG_DEPENDS}")
+    add_dependencies(${target} ${ARG_DEPENDS})
+  endif()
+
+  # Tests should be excluded from "Build Solution".
+  set_target_properties(${target} PROPERTIES EXCLUDE_FROM_DEFAULT_BUILD ON)
+endfunction()
+
+# A function to add a set of lit test suites to be driven through 'check-*' targets.
+function(add_lit_testsuite target comment)
+  cmake_parse_arguments(ARG "" "" "PARAMS;DEPENDS;ARGS" ${ARGN})
+
+  # EXCLUDE_FROM_ALL excludes the test ${target} out of check-all.
+  if(NOT EXCLUDE_FROM_ALL)
+    # Register the testsuites, params and depends for the global check rule.
+    set_property(GLOBAL APPEND PROPERTY OPOV_LIT_TESTSUITES ${ARG_UNPARSED_ARGUMENTS})
+    set_property(GLOBAL APPEND PROPERTY OPOV_LIT_PARAMS ${ARG_PARAMS})
+    set_property(GLOBAL APPEND PROPERTY OPOV_LIT_DEPENDS ${ARG_DEPENDS})
+    set_property(GLOBAL APPEND PROPERTY OPOV_LIT_EXTRA_ARGS ${ARG_ARGS})
+  endif()
+
+  # Produce a specific suffixed check rule.
+  add_lit_target(${target} ${comment}
+    ${ARG_UNPARSED_ARGUMENTS}
+    PARAMS ${ARG_PARAMS}
+    DEPENDS ${ARG_DEPENDS}
+    ARGS ${ARG_ARGS}
+    )
+endfunction()
--- cmake/modules/FindClang.cmake
+++ cmake/modules/FindClang.cmake
@@ -11,7 +11,11 @@ if(NOT LLVM_LIBRARY_DIRS)
   message(FATAL_ERROR "No LLVM library directory set.")
 else()  
   macro(find_and_add_clang_lib _libname_)
-    find_library(CLANG_${_libname_}_LIB ${_libname_} ${LLVM_LIBRARY_DIRS} ${CLANG_LIB_DIR})
+    find_library(CLANG_${_libname_}_LIB 
+      ${_libname_} 
+      PATHS ${LLVM_LIBRARY_DIRS} 
+            ${CLANG_LIB_DIR}
+      NO_DEFAULT_PATH)
     if(CLANG_${_libname_}_LIB)
       set(CLANG_LIBS ${CLANG_LIBS} ${CLANG_${_libname_}_LIB})
     else()
