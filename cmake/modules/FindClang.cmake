0b0b439eb3e01f3ba9a60ab26db780a6f6760c20
Alexander Hück <hueck.alexander@gmail.com>
2017-03-22 16:40:35 +0100

    Small refactoring

--- cmake/modules/FindClang.cmake
+++ cmake/modules/FindClang.cmake
@@ -7,8 +7,8 @@
 #  - ${CLANG_LIBS}: contains all found Clang libraries for linking
 #  - ${CLANG_FOUND}: true if (all) clang libraries were found
 #
-if(NOT LLVM_LIBRARY_DIRS)
-  message(FATAL_ERROR "No LLVM library directory set.")
+if(NOT LLVM_LIBRARY_DIRS AND NOT CLANG_LIB_DIR)
+  message(FATAL_ERROR "No LLVM/Clang library directory set.")
 else()  
   macro(find_and_add_clang_lib _libname_)
     find_library(CLANG_${_libname_}_LIB 
@@ -16,6 +16,7 @@ else()
       PATHS ${LLVM_LIBRARY_DIRS} ${CLANG_LIB_DIR}
       NO_DEFAULT_PATH
     )
+
     if(CLANG_${_libname_}_LIB)
       set(CLANG_LIBS ${CLANG_LIBS} ${CLANG_${_libname_}_LIB})
     else()
@@ -23,26 +24,32 @@ else()
     endif()
   endmacro()
 
-  find_and_add_clang_lib(clangFrontend)
-  find_and_add_clang_lib(clangDriver)
-  find_and_add_clang_lib(clangCodeGen)
-  find_and_add_clang_lib(clangSema)
-  find_and_add_clang_lib(clangAnalysis)
-  find_and_add_clang_lib(clangRewrite)
-  find_and_add_clang_lib(clangAST)
-  find_and_add_clang_lib(clangParse)
-  find_and_add_clang_lib(clangLex)
-  find_and_add_clang_lib(clangBasic)
-  find_and_add_clang_lib(clangARCMigrate)
-  find_and_add_clang_lib(clangEdit)
-  find_and_add_clang_lib(clangFrontendTool)
-  find_and_add_clang_lib(clangRewrite)
-  find_and_add_clang_lib(clangSerialization)
-  find_and_add_clang_lib(clangTooling)
-  find_and_add_clang_lib(clangASTMatchers)
-  find_and_add_clang_lib(clangSema)
-  find_and_add_clang_lib(clangToolingCore)
-  find_and_add_clang_lib(clangTooling)
+  set(CLANG_LIB_NAMES 
+    clangFrontend
+    clangDriver
+    clangCodeGen
+    clangSema
+    clangAnalysis
+    clangRewrite
+    clangAST
+    clangParse
+    clangLex
+    clangBasic
+    clangARCMigrate
+    clangEdit
+    clangFrontendTool
+    clangRewrite
+    clangSerialization
+    clangTooling
+    clangASTMatchers
+    clangSema
+    clangToolingCore
+    clangTooling
+  )
+  
+  foreach(clang_lib ${CLANG_LIB_NAMES})
+    find_and_add_clang_lib(${clang_lib})
+  endforeach()
 
   if(CLANG_LIBS)
     set(CLANG_FOUND TRUE)
